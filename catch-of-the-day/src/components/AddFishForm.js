daae1e528725ccef682ea00c631e68589364110d
hughdevore <hughdevore@gmail.com>
2018-09-25 11:21:27 -0600

    Stashing changes.

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -1,15 +1,47 @@
 import React from 'react';
 
-class Inventory extends React.Component {
+class AddFishForm extends React.Component {
+
+    nameRef = React.createRef();
+    priceRef = React.createRef();
+    statusRef = React.createRef();
+    descRef = React.createRef();
+    imageRef = React.createRef();
+
+    createFish = (e) => {
+        // Stop the form from submitting
+        e.preventDefault();
+
+        const fish = {
+            name: this.nameRef.value.value,
+            price: parseFloat(this.priceRef.value.value),
+            status: this.statusRef.value.value,
+            desc: this.descRef.value.value,
+            image: this.imageRef.value.value
+        }
+
+        // Add the fish to the state
+       this.props.addFish(fish);
+
+       // Reset the form
+       e.currentTarget.reset();
+    } 
+
     render() {
         return(
-            <div className="add-fish-form">
-                <form>
-                    
-                </form>
-            </div>
+            <form className="fish-edit" onSubmit={this.createFish}>
+                <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
+                <input name="price" ref={this.priceRef} type="text" placeholder="Price" />
+                <select name="status" ref={this.statusRef} >
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out!</option>
+                </select>
+                <textarea name="desc" ref={this.descRef} placeholder="Desc" />
+                <input name="image" ref={this.imageRef} type="text" placeholder="Image" />
+                <button type="submit">+ Add Fish</button>
+            </form>
         )
     }
 }
 
-export default Inventory;
+export default AddFishForm;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,6 +5,24 @@ import Inventory from './Inventory';
 import '../css/style.css';
 
 class App extends React.Component {
+
+    state = {
+        fishes: {},
+        order: {}
+    }
+
+    addFish = (fish) => {
+        // Make a copy of the existing state
+        const fishes = { ...this.state.fishes };
+
+        // Add the new fish to the fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+
+        // Set the new fishes object to state
+        this.setState({ fishes });
+
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -12,7 +30,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Daily"/>
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} />
             </div>
         )
     }
